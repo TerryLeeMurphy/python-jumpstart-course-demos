7306618fa5c4ed2c1fb96c2e0b91ac051fe1cd0d
Terry Murphy <33553527+TerryLeeMurphy@users.noreply.github.com>
2020-10-11 08:30:45 -0500

    Made headers.py enhancements to streamline code

---
+++ apps/06_lolcat_factory/you_try/cats.py
@@ -0,0 +1,13 @@
+import headers
+
+# Globals
+ds =72  # Console Width for Display
+title = ' C u t e  C a t s '
+
+def main():
+    headers.print_header(title,ds)
+
+
+
+if __name__ == '__main__':
+    main()
